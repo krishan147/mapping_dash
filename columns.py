bbdfdd9440ea0d547869da261e8bdd428ae91d55
krishan vadher <krishan.vadher@gmail.com>
2020-07-24 16:03:06 +0100

    info bit not working

--- columns.py
+++ columns.py
@@ -28,12 +28,11 @@ marks = [0, 10, 20, 50, 100, 200, 500, 1000]
 colorscale = ['#FFEDA0', '#FED976', '#FEB24C', '#FD8D3C', '#FC4E2A', '#E31A1C', '#BD0026', '#800026']
 
 def get_style(feature):
-    print ("get_style ran")
     color = [colorscale[i] for i, item in enumerate(marks) if feature["properties"]["Footfall"] > item][-1] #area_hectares
-    return dict(fillColor=color, weight=2, opacity=1, color='white', dashArray='3', fillOpacity=0.7)
+    that = dict(fillColor=color, weight=2, opacity=1, color='white', dashArray='3', fillOpacity=0.7)
+    return that
 
 def get_info(feature=None):
-    print ("get_info ran")
     header = [html.H4("London")]
     if not feature:
         return header + ["Hoover over map"]
@@ -43,7 +42,7 @@ def get_info(feature=None):
 ctg = ["{}+".format(mark, marks[i + 1]) for i, mark in enumerate(marks[:-1])] + ["{}+".format(marks[-1])]
 options = dict(hoverStyle=dict(weight=5, color='#666', dashArray=''), zoomToBoundsOnClick=True)
 geojson = dlx.geojson(data, id="geojson", defaultOptions=options, style=get_style)
-# Create info control.
+
 info = html.Div(children=get_info(), id="info", className="info",
                 style={"position": "absolute", "top": "10px", "right": "10px", "z-index": "1000"})
 
@@ -82,12 +81,24 @@ app.layout = html.Div(
     Output(component_id='radio_output', component_property='children'),
     [Input(component_id='radioitems', component_property='value')])
 def update_radio(input_value):
+    if input_value != None:
 
-    if input_value == "Total_Sales":
+        def get_style(feature):
+            color = [colorscale[i] for i, item in enumerate(marks) if feature["properties"][input_value] > item][-1]
+            return dict(fillColor=color, weight=2, opacity=1, color='white', dashArray='3', fillOpacity=0.7)
 
-        print (data["properties"]) # for some reason this wont print wtf?! krishan
+        def get_info(feature=None):
+            header = [html.H4("London")]
+            if not feature:
+                return header + ["Hoover over map"]
+            return header + [html.B(feature["properties"]["Name"]), html.Br(),
+                             "{} per hour".format(feature["properties"][input_value])]
 
-        #color = [colorscale[i] for i, item in enumerate(marks) if data["properties"]["Footfall"] > item][-1]  # area_hectares
+        options = dict(hoverStyle=dict(weight=5, color='#666', dashArray=''), zoomToBoundsOnClick=True)
+        geojson = dlx.geojson(data, id="geojson", defaultOptions=options, style=get_style)
+
+        info = html.Div(children=get_info(), id="info", className="info",
+                        style={"position": "absolute", "top": "10px", "right": "10px", "z-index": "1000"})
 
         return html.Div([
                     dl.Map(children=[dl.TileLayer(), geojson, info], center=[51.51, -0.083], zoom=12)
@@ -95,13 +106,28 @@ def update_radio(input_value):
                     style={'width': '95%', 'height': '75vh', 'margin': "auto", "display": "block"}, id="map",
                     className= 'six columns'
                 )
-    else:
-
-        return [html.Div(id='radio_output', style={"margin-left":"15px"})]
 
 @app.callback(Output("info", "children"), [Input("geojson", "featureHover")])
+
+
+# the below doesnt work for some reason krishan
 def info_hover(feature):
-    return get_info(feature)
+
+    def get_info(feature=None):
+        header = [html.H4("London")]
+        if not feature:
+            return header + ["Hoover over map"]
+        return header + [html.B(feature["properties"]["Name"]), html.Br(),
+                         "{} per hour".format(feature["properties"]["Footfall"])]
+
+    info = html.Div(children=get_info(), id="info", className="info",
+                    style={"position": "absolute", "top": "10px", "right": "10px", "z-index": "1000"})
+
+    return html.Div([info])
+
+    # return html.Div([
+    #                 dl.Map(children=[info], center=[51.51, -0.083], zoom=12)
+    #             ])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
